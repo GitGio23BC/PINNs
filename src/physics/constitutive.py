2e9c9314c5c1b54b3a5ac10663b2227f962cfe41
GitGio23BC <schiavella.2062070@studenti.uniroma1.it>
2026-08-26 16:06:24 +0100

    # Torchcify Phisycs model

--- src/physics/constitutive.py
+++ src/physics/constitutive.py
@@ -1,7 +1,11 @@
 from abc import ABC, abstractmethod
+
 import numpy as np
+import torch
 
 
+# fmt: off
+# ruff: disable[PIE790]
 class ConstitutiveModel(ABC):
     """
     Abstract base class for hyperelastic constitutive models.
@@ -45,3 +49,46 @@ class ConstitutiveModel(ABC):
             )
 
         return (P @ F.T) / J
+# fmt: on
+# ruff: enable[PIE790]
+
+
+class STrainEnergy(ABC):
+    """
+    Abstract base class for Strain Energy function.
+    It is important to notice that the s- is privative
+    so it is represent a Energy that if applied
+    can remove the Train. From that STrainEnergy.
+
+    Every constitutive model must provide:
+
+        - strain_energy(E)
+        - strain_gradient(E)
+        - strain_hessian(E)
+
+    where F is the deformation gradient.
+    """
+
+    @abstractmethod
+    def energy(self, E: torch.Tensor) -> torch.Tensor:
+        """
+        Strain Energy Function
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def grad(self, E: torch.Tensor) -> torch.Tensor:
+        """
+        Strain Energy Gradient
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def hessian(self, E: torch.Tensor) -> torch.Tensor:
+        """
+        Strain Energy Hession
+        """
+        raise NotImplementedError
+
+
+class 
